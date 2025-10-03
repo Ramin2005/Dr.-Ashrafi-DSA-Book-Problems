0d48005c466954c25df183cba4ce64261b44783e
Ramin2005 <riranmanesh05@gmail.com>
2025-10-03 12:31:21 +0330

    problem 2

--- Chapter 1/Problem 2/Ch1_Problem2.cpp
+++ Chapter 1/Problem 2/Ch1_Problem2.cpp
@@ -1,1 +1,7 @@
+double f(int Arr[], int n)
+{
+    if (n == 1)
+        return Arr[0];
 
+    return (Arr[(n - 1)] / sizeof(Arr)) + f(Arr, (n - 1));
+}
--- Chapter 1/Problem 2/Ch1_Problem2.java
+++ Chapter 1/Problem 2/Ch1_Problem2.java
@@ -1,1 +1,8 @@
+public class Ch1_Problem2 {
+    public static double f(int[] Arr, int n) {
+        if (n == 1)
+            return Arr[0];
 
+        return (Arr[(n - 1)] / Arr.length) + f(Arr, (n - 1));
+    }
+}
