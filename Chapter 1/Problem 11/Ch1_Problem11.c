72a50a71456c7e52bbfdd729777c6534901a02dc
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-10-22 19:54:14 +0330

    Problem 11

--- Chapter 1/Problem 11/Ch1_Problem11.c
+++ Chapter 1/Problem 11/Ch1_Problem11.c
@@ -2,8 +2,15 @@
 
 void Combination(int n, int num10, int num5, int num2)
 {
-    if (n = 0)
+    if (n == 0)
     {
+        if (num10 == -1)
+            num10 = 0;
+        if (num5 == -1)
+            num5 = 0;
+        if (num2 == -1)
+            num2 = 0;
+            
         printf("\n%d %d %d", num10, num5, num2);
         return;
     }
@@ -11,5 +18,35 @@ void Combination(int n, int num10, int num5, int num2)
     if (n < 2)
         return;
 
-    
+    if (num10 == -1)
+    {
+        int temp = n / 10;
+        for (int i = 0; i <= temp; i++)
+        {
+            Combination((n - (10 * i)), i, num5, num2);
+        }
+    }
+
+    if (num5 == -1)
+    {
+        if (num10 == -1)
+            num10 = 0;
+        int temp = n / 5;
+        for (int i = 0; i <= temp; i++)
+        {
+            Combination((n - (5 * i)), num10, i, num2);
+        }
+    }
+
+    if (num2 == -1)
+    {
+        if (num5 == -1)
+            num5 = 0;
+        Combination((n % 2), num10, num5, (n / 2));
+    }
+}
+
+int main()
+{
+    Combination(20, -1, -1, -1);
 }
