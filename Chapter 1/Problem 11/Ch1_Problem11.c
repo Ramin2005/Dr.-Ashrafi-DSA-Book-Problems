c65b8398d34c217d8c91750542ec32db1a749329
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-10-22 19:34:33 +0330

    Problem 11

--- Chapter 1/Problem 11/Ch1_Problem11.c
+++ Chapter 1/Problem 11/Ch1_Problem11.c
@@ -1,12 +1,10 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 void Combination(int n, int num10, int num5, int num2)
 {
     if (n = 0)
     {
-        cout << "\n"
-             << num10 << " " << num5 << " " << num2;
+        printf("\n%d %d %d", num10, num5, num2);
         return;
     }
 
