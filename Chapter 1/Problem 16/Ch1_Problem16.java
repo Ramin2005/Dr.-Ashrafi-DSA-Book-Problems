7243d188e847c628bcfe88197a4065766b722ce7
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-10-27 11:18:33 +0330

    Problem 16

--- Chapter 1/Problem 16/Ch1_Problem16.java
+++ Chapter 1/Problem 16/Ch1_Problem16.java
@@ -6,11 +6,11 @@ public class Ch1_Problem16 {
 
     public static void SubSets(ArrayList<Integer> set, Set<ArrayList<Integer>> result) {
         if (set.isEmpty()) {
-            result.add(new ArrayList<Integer>());
+            result.add(new ArrayList<>());
         } else {
             result.add(set);
             for (int i = 0; i < set.size(); i++) {
-                ArrayList<Integer> newSet = new ArrayList<Integer>(set);
+                ArrayList<Integer> newSet = new ArrayList<>(set);
                 newSet.remove(i);
                 SubSets(newSet, result);
             }
