55859e7d593f5e4d7866568cc08e0ff0ad1faaa6
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-11-07 13:46:12 +0330

    Problem 1

---
+++ Chapter 3/Problem 1/Ch1_Problem.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+long fib(int n)
+{
+
+    if (n < 2)
+    {
+        return 1;
+    }
+
+    long fibs[(n + 1)];
+
+    fibs[0] = 1;
+    fibs[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+
+        fibs[i] = fibs[(i - 1)] + fibs[(i - 2)];
+    }
+
+    return fibs[n];
+}
