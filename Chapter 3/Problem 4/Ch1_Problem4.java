77032b746eb5dd69984d4ef858952cfcdf0a2cef
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-11-22 18:11:38 +0330

    Update Ch1_Problem4.java

--- Chapter 3/Problem 4/Ch1_Problem4.java
+++ Chapter 3/Problem 4/Ch1_Problem4.java
@@ -1,4 +1,12 @@
 
+
 public class Ch1_Problem4 {
     
+    public BigNumber fac(int n){
+        BigNumber result = new BigNumber("1");
+        for(int i = 2; i <= n; i++){
+            result = result.multiply(new BigNumber(Integer.toString(i)));
+        }
+        return result;
+    }
 }
