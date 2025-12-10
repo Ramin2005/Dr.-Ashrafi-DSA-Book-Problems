24570e8ec2550d2f357a9e54af1317bb5290875b
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-12-10 18:29:44 +0330

    Update Ch4_Problem3.java

--- Chapter 4/Problem 3/Ch4_Problem3.java
+++ Chapter 4/Problem 3/Ch4_Problem3.java
@@ -1,4 +1,91 @@
 
 public class Ch4_Problem3 {
 
+    public class Queue {
+
+        private Node data;
+        private Node lastNode;
+        private int size;
+
+        public Queue() {
+            this.size = 0;
+        }
+
+        public void enQueue(Object value) {
+
+            if (this.size == 0) {
+
+                this.data = new Node(value);
+                this.lastNode = this.data;
+                ++this.size;
+
+            } else {
+
+                this.lastNode.setNextNode(new Node(value));
+                this.lastNode = this.lastNode.getNextNode();
+                ++this.size;
+
+            }
+
+        }
+
+        public Object deQueue() {
+            if (this.isEmpty()) {
+                throw new RuntimeException("Queue is Empty1");
+            }
+
+            Object temp = this.data.getValue();
+
+            this.data = this.data.getNextNode();
+            --this.size;
+
+            return temp;
+        }
+
+        public boolean isEmpty() {
+            return this.size == 0;
+        }
+
+        public int Size() {
+            return size;
+        }
+
+        public class Node {
+
+            private Object value;
+            private Node nextNode = null;
+
+            public Node() {
+
+            }
+
+            public Node(Object value) {
+                this.value = value;
+            }
+
+            public Node(Object value, Node nextNode) {
+                this.value = value;
+                this.nextNode = nextNode;
+            }
+
+            public Object getValue() {
+                return value;
+            }
+
+            public boolean hasNextNode() {
+                return this.nextNode != null;
+            }
+
+            public Node getNextNode() {
+                return nextNode;
+            }
+
+            public void setNextNode(Node nextNode) {
+                this.nextNode = nextNode;
+            }
+
+        }
+
+    }
+
 }
