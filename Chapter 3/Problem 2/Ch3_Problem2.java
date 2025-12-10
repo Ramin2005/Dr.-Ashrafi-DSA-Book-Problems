b1c6324c6bf67b1d6a99515d619edcc357187472
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-12-10 15:33:47 +0330

    Update Ch3_Problem2.java

--- Chapter 3/Problem 2/Ch3_Problem2.java
+++ Chapter 3/Problem 2/Ch3_Problem2.java
@@ -1,4 +1,35 @@
 
 public class Ch3_Problem2 {
-    
+
+    public static long c(int n, int r) {
+
+        if (r > n) {
+
+            return 0;
+
+        }
+
+        long[][] list = new long[n + 1][n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+
+            list[i][0] = 1;
+
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+
+            for (int j = 1; j < i + 1; j++) {
+
+                list[i][j] = list[i - 1][j - 1] + list[i - 1][j];
+
+            }
+
+        }
+
+        list[n][n] = 1;
+
+        return list[n][r];
+    }
+
 }
