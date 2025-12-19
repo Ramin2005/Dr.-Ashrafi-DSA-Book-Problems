0bc1f8585e01bc5ed088d83c8c9780ffc65b3013
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-12-19 17:53:54 +0330

    Create Ch5_Problem11.java

---
+++ Chapter 5/Problem 11/Ch5_Problem11.java
@@ -0,0 +1,52 @@
+
+import java.util.Stack;
+
+public class Ch5_Problem11 {
+
+    public class Queue {
+
+        private Stack<Object> stack = new Stack<>();
+        private int size;
+
+        public Queue() {
+            this.size = 0;
+        }
+
+        public void enQueue(Object value) {
+
+            this.stack.push(value);
+            ++this.size;
+
+        }
+
+        public Object deQueue() throws Exception {
+
+            if (this.size == 0) {
+
+                throw new Exception("Queue is empty!");
+
+            }
+
+            Object temp;
+
+            Stack<Object> helperStack = new Stack<>();
+
+            while (!this.stack.isEmpty()) {
+
+                helperStack.push(this.stack.pop());
+
+            }
+
+            temp = helperStack.pop();
+
+            while (!helperStack.isEmpty()) {
+
+                this.stack.push(helperStack.pop());
+
+            }
+
+            return temp;
+        }
+
+    }
+}
